3d00dfcd27789f64945ab232a910e4dac863bb5e
Vishnu Srinivasrao Biradar <vishnu.biradar@yash.com>
2022-02-02 13:52:34 +0530

    add The forst hook test case state

--- src/componets/jotto/Input.js
+++ src/componets/jotto/Input.js
@@ -3,7 +3,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function Input({ secrectWord }) {
-    return <div data-test="component-input"></div>;
+    const [currentGuess, setCurrentGuess] = React.useState("");
+    return <div data-test="component-input">
+        <form className='form-inline'>
+            <input
+                data-test='input-box'
+                className='mb-2 mx-sm-3'
+                type="text"
+                placeholder='Enter guess'
+                value={currentGuess}
+                onChange={(event) => setCurrentGuess(event.target.value)}
+            />
+            <button
+                data-test="submit-button"
+                className='btn btn-primary mb-2'
+            >Submit</button>
+        </form>
+
+    </div>;
 }
 
 Input.propTypes = {
--- src/test/components/jotto/Input.test.js
+++ src/test/components/jotto/Input.test.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { shallow } from 'enzyme';
 import { findByTestAttr, checkProp } from '../../tesrUtilis';
 import Input from '../../../componets/jotto/Input';
@@ -17,4 +17,20 @@ test('render without error', () => {
 
 test('does not throw warning with expected props', () => {
     checkProp(Input, {secrectWord :'party'});
+})
+
+describe('state controlled input Field',()=>{
+
+    test('state update with value of input box upon change',()=>{
+        const mockSetCurrentGuess =jest.fn();
+        React.useState=jest.fn(()=>["", mockSetCurrentGuess]);
+        const wrapper=setUp();
+        const inputBox=findByTestAttr(wrapper, "[data-test='input-box']");
+
+        const mockEvent={ target: {value:'train'}}
+
+        inputBox.simulate("change",mockEvent);
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    })
+
 })
